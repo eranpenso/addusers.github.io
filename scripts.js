579371497082f8e2051e846586cf19e92a81fa20
eranpenso <72167057+eranpenso@users.noreply.github.com>
2022-02-25 17:54:46 +0200

    added multi language

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
 const button = document.getElementById("submit_button");
+//LANGUAGES
+const lang_heb = {
+    title: "תודה לך שנרשמת!",
+    sub_title: "עכשיו נשאר רק להכניס פרטים, והבוט מיד שולח לך הודעה!",
+    mail: "המייל שאיתו שילמת (בפייפאל)",
+    name: "שם",
+    phone_number: "מספר טלפון",
+    button_text: "תרשמו אותי"
+}
+const lang_en = {
+    title: "Thank you for signing up!",
+    sub_title: "Now all that is left is to enter details, and the bot will immediately send you a message",
+    mail: "The email you paid with (on PayPal)",
+    name: "Name",
+    phone_number: "Phone number",
+    button_text: "Submit!"
+}
+
+//LANG SELECTION CHANGED
+document.getElementById("language").onchange = function(){
+    if(this.value == "heb")
+        Render(lang_heb);
+    if(this.value == "en")
+        Render(lang_en);
+}
+
+//RENDER
+const Render = (lang) =>{
+    if(lang == lang_heb)
+        document.body.setAttribute("dir", "rtl");
+    else
+        document.body.setAttribute("dir", "ltr");
+    
+    document.getElementById("title").innerHTML = lang.title;
+    document.getElementById("sub_title").innerHTML = lang.sub_title;
+    document.getElementById("mail").placeholder = lang.mail;
+    document.getElementById("name").placeholder = lang.name;
+    document.getElementById("phoneNumber").placeholder = lang.phone_number;
+    document.getElementById("submit_button").innerHTML = lang.button_text;
+}
+
 
 window.onload = function(){
+    Render(lang_en);
     if(!document.referrer.includes("paypal")){
         alert("הדף זמין רק לאחר תשלום!");
         document.getElementById("submit_button").disabled = true;
